b187e50a61c1f23b4f91d27a9e740d9d647490e4
Taichi Endo <endotaichi1101@icloud.com>
2017-12-29 23:26:38 +0900

    Mainで決定を押したらCalc画面に行ける

---
+++ app/src/main/java/com/example/android/sample/keiba/CalcActivity.java
@@ -0,0 +1,24 @@
+package com.example.android.sample.keiba;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class CalcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+
+
+
+        //ここでやることは計算するためのパラメータを入力する項目を作ること
+        //横スライドで次の項目を入力できる。親指だけで楽に入力できるようにする
+
+    }
+
+}
