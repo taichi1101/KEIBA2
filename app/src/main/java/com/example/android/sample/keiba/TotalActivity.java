6c51bae1a48f5ecdebd9b29e04da150f5d023e6c
Taichi Endo <endotaichi1101@icloud.com>
2018-01-02 15:22:12 +0900

    能力表示画面を作った。レイアウトは、出走馬表と同じ仕組み

---
+++ app/src/main/java/com/example/android/sample/keiba/TotalActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.sample.keiba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TotalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_total);
+    }
+}
