38f9f84c7b33dc81ebdb21e537fd22de05a4d052
Taichi Endo <endotaichi1101@icloud.com>
2018-01-07 01:52:30 +0900

    全ての画面にボタンとspinnerをつけてだいぶ良くなった

--- app/src/main/java/com/example/android/sample/keiba/CalcActivity.java
+++ app/src/main/java/com/example/android/sample/keiba/CalcActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.sample.keiba;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,23 +11,55 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 
-
-
-public class CalcActivity extends AppCompatActivity {
+public class CalcActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ViewPager pager;
+
+
+    Button pushChatButton;
+    Button pushTotalButton;
+    Button pushMainButton;
+
+    //    レースのspinner
+    Spinner idRaceSpinner;
+
 
     private FragmentPagerAdapter adapter;
 
     private int currentPage;
+    private String TAG="CalcActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
 
+
+        pushChatButton=(Button)findViewById(R.id.pushChatButton);
+        pushTotalButton=(Button)findViewById(R.id.pushTotalButton);
+        pushMainButton=(Button)findViewById(R.id.pushMainButton);
+
+
+        pushChatButton.setOnClickListener(this);
+        pushTotalButton.setOnClickListener(this);
+        pushMainButton.setOnClickListener(this);
+
+
+
+
+//        spinner
+        idRaceSpinner = (Spinner) findViewById(R.id.raceSpinner);
+
+        firstRaceSpinner();
 
 
         pager = (ViewPager) findViewById(R.id.pager);
@@ -36,6 +69,117 @@ public class CalcActivity extends AppCompatActivity {
         currentPage = 0;
 
     }
+
+
+
+    String raceSpinner[];
+    public void firstRaceSpinner(){
+
+
+        raceSpinner = new String[5];
+        raceSpinner[0] = "桜花賞";
+        raceSpinner[1] = "日本ダービー";
+        raceSpinner[2] ="菊花賞";
+        raceSpinner[3] ="天皇賞・秋";
+        raceSpinner[4] ="有馬記念";
+
+//        この下のコードがあるとlistが表示されなくなり、ボタンが押せなくなる。とりあえず省いておく
+        //このしたが、キーボードが押されないようにしてる
+        //getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        //setContentView(R.layout.activity_chat);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.tool_bar);
+        // setSupportActionBar(toolbar);
+        //toolbar.setTitle("");
+        // mResolvingError = false;
+        //nullになるから、ここでよんどく
+
+
+//        spinnerを使うためここでセットする。セットしないとnullになる
+        raceSpinnerAdapterSet();
+
+    }
+
+
+    //    名前が変な気もするが、spinnerItemsを更新した時に呼び出す
+    public void raceSpinnerAdapterSet() {
+        ArrayAdapter<String> raceSpinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,raceSpinner);
+        raceSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        idRaceSpinner.setAdapter(raceSpinnerAdapter);
+        idRaceSpinner.setFocusable(false);
+
+    }
+
+    public void onStart() {
+        super.onStart();
+
+
+//        レースのspinnerが押された時の対応
+        idRaceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Spinner nowRaceSpinner = (Spinner) parent;
+                String item = (String) nowRaceSpinner.getSelectedItem();
+
+                //クラス変数に入れる。今選択されていたspinnerを後で指定できるように
+                int nowRaceSpinnerPosition = position;
+
+
+//                spinner初回起動
+                if (idRaceSpinner.isFocusable() == false) {
+                    idRaceSpinner.setFocusable(true);
+                    String activity = getIntent().getStringExtra("Activity");
+                    if (item.equals("桜花賞")) {
+
+                        Log.e(TAG, "1/6:初期化のspinnerで桜花賞が選択された");
+                    }
+                    return;
+                }
+
+//                選択を検知したspinnerごとの対応
+                if (item.equals("日本ダービー")) {
+                    String activity = getIntent().getStringExtra("Activity");
+
+                    Log.e(TAG, "1/6:初期化のspinnerで日本ダービーが選択された");
+                }
+            }
+            //アイテムが選択されなかった
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+//        spinnerItems = favorite.favorite(LocationActivity.this, username);//もしかしたら役に立つから、コメントを残す
+
+    }
+
+
+
+        @Override
+    public void onClick(View view) {
+
+
+        switch (view.getId()) {
+            case R.id.pushChatButton:
+
+                //次のデータ入力画面にいくボタンが押されたので、処理する
+                Intent intent = new Intent(this, ChatActivity.class);
+                startActivity(intent);
+                break;
+
+            //トータルスコアをみる
+            case R.id.pushTotalButton:
+                intent = new Intent(this, TotalActivity.class);
+                startActivity(intent);
+                break;
+
+            case R.id.pushMainButton:
+                intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+    }
+
 //ここをなくすと、余計なボタンがなくなっていい
 //    public void onClickNext(View view) {
 //        currentPage ++;
